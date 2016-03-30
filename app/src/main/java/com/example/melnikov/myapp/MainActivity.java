fafcd08c00e662d2a3c54f41992d4ec820d6f211
netcat <alexandr.teh@gmail.com>
2016-03-30 15:57:49 +0300

    Create MainActivite, activity_main

---
+++ app/src/main/java/com/example/melnikov/myapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.melnikov.myapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by MelnikOV on 030 30.03.16.
+ */
+public class MainActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
